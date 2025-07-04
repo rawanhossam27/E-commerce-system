db7750c173ec5d0edda4d785280a7479c31ebd88
rawanhossam27 <rawanmansour2004@gmail.com>
2025-07-05 00:16:25 +0300

    made shippable products implement the shippable interface

--- ShippedProducts.java
+++ ShippedProducts.java
@@ -1,9 +1,20 @@
-public class ShippedProducts extends Product{
+public class ShippedProducts extends Product implements Shippable {
     private double weight;
 
     public ShippedProducts(String name, double price, int quantity, double weight) {
         super(name, price, quantity);
         this.weight = weight;
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
     
     }
     @Override

867f3652e9f78737f9b1936b1cd53efd5d9c69f9
rawanhossam27 <rawanmansour2004@gmail.com>
2025-07-05 01:51:23 +0300

    solved errors in cart class

--- Cart.java
+++ Cart.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Cart {
-    private Map<Product, amount> items;
+    private Map<Product, Integer> items;
 
     public Cart() {
         items = new HashMap<>();
     }
 
-    public Map<Product, amount> getItems() {
+    public Map<Product, Integer> getItems() {
         return items;
     }
 
@@ -28,12 +26,12 @@ public class Cart {
             items.put(product, currentQty + quantity);
         } else {
             items.put(product, quantity);
-                }
+        }
     }
 
     public Map<String, Integer> getProductNamesAndAmounts() {
         Map<String, Integer> result = new HashMap<>();
-        for (Map.Entry<Product, amount> entry : items.entrySet()) {
+        for (Map.Entry<Product, Integer> entry : items.entrySet()) {
             Product product = entry.getKey();
             int quantity = entry.getValue();
             result.put(product.getName(), quantity);
@@ -45,7 +43,7 @@ public class Cart {
         if (items.containsKey(product)) {
             return product.getName();
         }
-        return null; 
+        return null;
     }
 
     public Integer getProductAmount(Product product) {

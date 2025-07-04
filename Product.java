fd838379949b141f3ffd72f284cb19ee203c7870
rawanhossam27 <rawanmansour2004@gmail.com>
2025-07-05 00:04:22 +0300

    added initial variables and methods to product class

--- Product.java
+++ Product.java
@@ -1,2 +1,42 @@
 public class Product {
-}
+    private String name;
+    private double price;
+    private int quantity;
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+
+    public boolean isExpired(){
+        return false; //to be overritten in ExpirableProducts
+        }
+    public boolean isShippable() {
+        return false; // to be overridden in ShippableProducts
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}   
+    
+}  
